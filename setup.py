ed8713545fa398c696acb0242a3a88201aecbd25
Ajinkya Raskar <ajinkyaraskar@gmail.com>
2024-04-12 23:43:14 +0530

    another

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(filepath:str)->List[str]:
+    '''
+    this fn will return list of requirements
+    '''
+    requirements = []
+    with open(filepath) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Ajinkya',
+    author_email='ajinkyaraskar@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
